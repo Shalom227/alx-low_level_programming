96fee24efa0578ae24b75e6af52f9f8de0d443cf
Shalom <dreamtech227@gmail.com>
2023-02-17 03:11:13 -0800

    fixed error in task 6 and solved the last task 101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,15 @@
  */
 
 int main(void)
-
-{	
-	printf("Size of int: %ld bytes\n", sizeof(int));
-	printf("Size of unsigned char: %ld bytes\n", sizeof(unsigned char));
-	printf("Size of float: %ld bytes\n", sizeof(float));
-	printf("Size of char: %ld byte\n", sizeof(char));
-	printf("Size of short: %ld byte\n", sizeof(short));
-	printf("Size of unsigned short: %ld byte\n", sizeof(unsigned short));
-	printf("Size of unsigned int: %ld byte\n", sizeof(unsigned int));
-	printf("Size of unsigned long: %ld byte\n", sizeof(unsigned long));
-	printf("Size of long: %ld byte\n", sizeof(long));
+{
+	printf("Size of int:%ld bytes\n", sizeof(int));
+	printf("Size of unsigned char:%ld bytes\n", sizeof(unsigned char));
+	printf("Size of float:%ld bytes\n", sizeof(float));
+	printf("Size of char:%ld byte\n", sizeof(char));
+	printf("Size of short:%ld byte\n", sizeof(short));
+	printf("Size of unsigned short:%ld byte\n", sizeof(unsigned short));
+	printf("Size of unsigned int:%ld byte\n", sizeof(unsigned int));
+	printf("Size of unsigned long:%ld byte\n", sizeof(unsigned long));
+	printf("Size of long:%ld byte\n", sizeof(long));
 	return (0);
 }
