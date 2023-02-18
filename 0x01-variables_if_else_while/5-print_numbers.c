9126e8d6f48384166b60502727935e302e8bad89
Shalom <dreamtech227@gmail.com>
2023-02-18 08:06:29 -0800

    fixed errors in task 5 and 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
-
-		putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
 	return (0);
 }
