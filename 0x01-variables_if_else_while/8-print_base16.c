bfbd46f1ebb12485fcebc55dffdc4a3f85845fd1
Shalom <dreamtech227@gmail.com>
2023-02-18 07:10:09 -0800

    origin master

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	int num;
 	char lowercase;
 
-	for (num  = '0'; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (lowercase = 'a'; lowercase <= 'f'; lowercase++)
