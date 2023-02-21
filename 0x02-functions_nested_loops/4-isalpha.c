e29d31338337835ba64a171394659ba7d8738e14
Shalom <dreamtech227@gmail.com>
2023-02-21 05:44:11 -0800

    fixed error in task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - checks for alphabetic character
- * @c: Character c is to be checked
+ * _isalpha - checks for alphabetic character
+ * @c: Character c is what is checked
  * Return: 1 for lowercase character or 0 otherwise
 */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 122) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
