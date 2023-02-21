2b4e8f8b2c1b75fa14c3f62fc4c4248e4eaa0f51
Shalom <dreamtech227@gmail.com>
2023-02-21 04:27:44 -0800

    pleasing betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,14 @@
 /**
  * print_alphabet - prints alphabets in lower case 10x using _putchar
  * Return: Nothing
+ *
 */
 
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
-	/*we choose a variable name called c to represents all lowercase alphabets*/
+
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
